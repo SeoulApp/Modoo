7e56e4abea0f514969e9505b3ad4417925b472c9
msungkk218 <msungkk218@naver.com>
2018-09-21 10:54:55 +0900

    update 1.2

---
+++ app/src/main/java/com/minsung/examples/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.minsung.examples;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getBaseContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+
+    }
+}
