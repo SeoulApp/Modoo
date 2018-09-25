75098bbab57b670d71a35db2846e5656d26d6c0a
msungkk218 <msungkk218@naver.com>
2018-09-25 11:57:25 +0900

    update 1.3

---
+++ app/src/main/java/com/minsung/examples/CardAuthenticationActivity.java
@@ -0,0 +1,13 @@
+package com.minsung.examples;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CardAuthenticationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_authentication);
+    }
+}
